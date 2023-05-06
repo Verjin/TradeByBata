bcf45d0457a9d108467c1611384c324ef579d3da
Vlad Lysenko <vlad.lysenka@icloud.com>
2023-05-06 11:44:18 +0300

    Initial Commit

---
+++ TradeByBata/TradeByBataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeByBataApp.swift
+//  TradeByBata
+//
+//  Created by Влад on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TradeByBataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
